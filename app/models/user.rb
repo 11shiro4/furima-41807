49964ba8e3b7afbc83dfd817b867e2272ff3329c
11shiro4 <yumako345@gmail.com>
2024-12-14 20:36:27 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
