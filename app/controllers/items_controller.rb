943c7d7c83c6716b7b0f3beffdecc7882e8545b7
11shiro4 <yumako345@gmail.com>
2024-12-17 18:24:10 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,20 @@
 class ItemsController < ApplicationController
-  def index
-  end
-
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :schedule_deliver_id, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
-<<<<<<< Updated upstream
-# class Item < ApplicationRecord
-# belongs_to :user
-# end
-=======
 class Item < ApplicationRecord
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :schedule_deliver
+
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :schedule_deliver_id, presence: true
+  has_one_attached :image
 end
->>>>>>> Stashed changes
