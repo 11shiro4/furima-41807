c7bd67d44f9589fd563a2ce65a87803486aaa185
11shiro4 <yumako345@gmail.com>
2024-12-19 14:42:58 +0900

    テストコード#2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,16 @@ FactoryBot.define do
   factory :item do
     item_name     { 'サンプル商品' }
     description   { 'これはサンプル商品です。' }
-    category      { 'レディース' }
-    status        { '新品・未使用' }
-    shipping_cost { '送料込み（出品者負担)' }
-    prefecture    { '東京都' }
-    shipping_date { '1~2日で発送' }
+    category_id   { Category.first.id }
+    status_id     { Status.first.id }
+    shipping_cost_id { ShippingCost.first.id }
+    prefecture_id { Prefecture.first.id }
+    shipping_date_id { ShippingDate.first.id }
     price         { 500 }
     association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
