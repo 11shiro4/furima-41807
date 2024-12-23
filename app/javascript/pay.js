7e2329d9cb390d12a78839fddbfb781d5b1b8801
11shiro4 <yumako345@gmail.com>
2024-12-23 22:28:20 +0900

    Payjpトークンの生成とフォーム送信

--- app/javascript/pay.js
+++ app/javascript/pay.js
@@ -2,18 +2,25 @@ const pay = () => {
   console.log("OK");
   const payjp = Payjp("pk_test_57e8e3d0c1b46cf341aaea64");
 
-  const token = response.id;
-  const renderDom = document.getElementById("charge-form");
-  const tokenObj = `<input value=${token} type="hidden" name='token'>`;
-  renderDom.insertAdjacentHTML("beforeend", tokenObj);
+  const form = document.getElementById("charge-form");
 
-    numberElement.clear();
-    expiryElement.clear();
-    cvcElement.clear();
-    document.getElementById("charge-form").submit();
+  payjp.createToken(numberElement).then(function (response) {
+    if (response.error) {
+      console.error(response.error.message);
+      alert(response.error.message);
+    } else {
+      const token = response.id;
+      const renderDom = document.getElementById("charge-form");
+      const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+      renderDom.insertAdjacentHTML("beforeend", tokenObj);
+
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+
+      document.getElementById("charge-form").submit();
+    }
+  });
 };
 
 window.addEventListener("turbo:load", pay);
-
-
-
