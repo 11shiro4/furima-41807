2be21220f6d067a50d6f17b2d89fa1845ff638d4
11shiro4 <yumako345@gmail.com>
2024-12-23 17:55:55 +0900

    テストコードの修正

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,84 +1,10 @@
-require 'rails_helper'
-
-RSpec.describe Order, type: :model do
-  before do
-    @user = FactoryBot.create(:user)
-    @item = FactoryBot.create(:item)
-    @order = FactoryBot.build(:order, user_id: @user.id, item_id: @item.id)
+FactoryBot.define do
+  factory :order do
+    postcode { '123-4567' }
+    prefecture_id { 1 }
+    city { '渋谷区' }
+    block { '1-1-1' }
+    building { 'テストビル' }
+    phone_number { '09012345678' }
+    token { 'tok_abcdefghijk00000000000000000' }
   end
-
-  describe '注文の保存' do
-    context '注文が保存できる場合' do
-      it 'すべての値が正しく入力されていれば保存できること' do
-        expect(@order).to be_valid
-      end
-
-      it '建物名が空でも保存できること' do
-        @order.building = ''
-        expect(@order).to be_valid
-      end
-    end
-
-    context '注文が保存できない場合' do
-      it '郵便番号が空では保存できないこと' do
-        @order.postcode = ''
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Postcode can't be blank")
-      end
-
-      it '郵便番号にハイフンがなければ保存できないこと' do
-        @order.postcode = '1234567'
-        @order.valid?
-        expect(@order.errors.full_messages).to include('Postcode は「3桁ハイフン4桁」の半角文字列で入力してください')
-      end
-
-      it '都道府県が空では保存できないこと' do
-        @order.prefecture_id = ''
-        @order.valid?
-        expect(@order.errors.full_messages).to include('Prefecture must be other than 1')
-      end
-
-      it '市区町村が空では保存できないこと' do
-        @order.city = ''
-        @order.valid?
-        expect(@order.errors.full_messages).to include("City can't be blank")
-      end
-
-      it '番地が空では保存できないこと' do
-        @order.block = ''
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Block can't be blank")
-      end
-
-      it '電話番号が空では保存できないこと' do
-        @order.phone_number = ''
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Phone number can't be blank")
-      end
-
-      it '電話番号が9桁以下では保存できないこと' do
-        @order.phone_number = '090123456'
-        @order.valid?
-        expect(@order.errors.full_messages).to include('Phone number は10桁以上11桁以内の半角数値で入力してください')
-      end
-
-      it '電話番号が12桁以上では保存できないこと' do
-        @order.phone_number = '090123456789'
-        @order.valid?
-        expect(@order.errors.full_messages).to include('Phone number は10桁以上11桁以内の半角数値で入力してください')
-      end
-
-      it '電話番号にハイフンが含まれていると保存できないこと' do
-        @order.phone_number = '090-1234-5678'
-        @order.valid?
-        expect(@order.errors.full_messages).to include('Phone number は10桁以上11桁以内の半角数値で入力してください')
-      end
-
-      it 'tokenが空では保存できないこと' do
-        @order.token = ''
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Token can't be blank")
-      end
-    end
-  end
-end
