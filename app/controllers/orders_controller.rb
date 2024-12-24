bdde13f7e1ae7a14ab2f71cca1a46b5caf3e87da
11shiro4 <yumako345@gmail.com>
2024-12-24 12:37:18 +0900

    ストロングパラメーター

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!, only: [:index, :create]
   before_action :redirect_if_invalid_access, only: [:index, :create]
+  before_action :set_payjp_public_key, only: [:index, :new]
 
   def index
     @order = Order.new
@@ -29,6 +30,10 @@ class OrdersController < ApplicationController
 
   private
 
+  def set_payjp_public_key
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+  end
+
   def charge_customer(token:, amount:)
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
@@ -49,7 +54,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number)
+    params.require(:order).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(token: params[:token])
   end
 
   def order_form_params
