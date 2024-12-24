1347d39a78f4fb2144a41d7773d2a7776a433905
11shiro4 <yumako345@gmail.com>
2024-12-24 15:08:52 +0900

    pay.jsの設定

--- app/javascript/pay.js
+++ app/javascript/pay.js
@@ -2,8 +2,17 @@ const pay = () => {
   console.log("OK");
   const payjp = Payjp("pk_test_57e8e3d0c1b46cf341aaea64");
   // const publicKey = gon.public_key
-
+  
   const form = document.getElementById("charge-form");
+  
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
 
   payjp.createToken(numberElement).then(function (response) {
     if (response.error) {
@@ -11,6 +20,8 @@ const pay = () => {
       alert(response.error.message);
     } else {
       const token = response.id;
+      console.log(token)
+
       const renderDom = document.getElementById("charge-form");
       const tokenObj = `<input value=${token} type="hidden" name='token'>`;
       renderDom.insertAdjacentHTML("beforeend", tokenObj);
